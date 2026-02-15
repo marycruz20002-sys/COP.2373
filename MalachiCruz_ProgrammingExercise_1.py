9f8ae2680297fbef0dc6e7ef0ef08840ae522b04
marycruz20002-sys <marycruz20002@gmail.com>
2026-02-15 12:40:47 -0500

    Add files via upload
    edits

--- MalachiCruz_ProgrammingExercise_1.py
+++ MalachiCruz_ProgrammingExercise_1.py
@@ -1,37 +1,37 @@
-def display_remaining(tickets_left):
-    """Function 1: Displays the remaining tickets."""
-    print(f"Remaining tickets: {tickets_left}")
+def display_remaining(stubs_left):
+    """Function 1: Displays the remaining stubs."""
+    print(f"Remaining stubs: {stubs_left}")
 
-def sell_tickets():
-    """Function 2: Manages the tickets selling logic."""
-    total_tickets = 20
+def sell_stubs():
+    """Function 2: Manages the stubs selling logic."""
+    total_stubs = 10
     buyers = 0    
     # Repeating loop until all stubs are sold
-    while total_tickets > 0:
-        display_remaining(total_tickets)
+    while total_stubs > 0:
+        display_remaining(total_stubs)
         try:
             # Input number of stubs
-            requested = int(input("Select amount of tickets to purchase (1-4): "))
+            requested = int(input("How many stubs would you like to purchase? (1-4): "))
             # Validation using if statement for incorrect number of tickets
             if requested < 1 or requested > 4:
-                print("Invalid amount. You can buy 1-4 tickets.")
+                print("Invalid amount. You can buy 1-4 stubs.")
                 continue
-            # Validation using if statement for insufficient tickets remaining
-            if requested > total_tickets:
-                print(f"Not enough tickets. Only {total_tickets} left.")
+            # Validation using if statement for insufficient stubs remaining
+            if requested > total_stubs:
+                print(f"Not enough tickets. Only {total_stubs} left.")
                 continue
                 
             # Update ticket count (accumulator)
-            total_tickets -= requested
+            total_stubs -= requested
             buyers += 1 # Accumulator for buyers
             
             # Output
-            print(f"Sold {requested} ticket(s).")
+            print(f"Sold {requested} stub(s).")
             
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-    print(f"All tickets sold! Total buyers: {buyers}")
+    print(f"All stubs sold! Total buyers: {buyers}")
 
 # Run the program
-sell_tickets()
+sell_stubs()
